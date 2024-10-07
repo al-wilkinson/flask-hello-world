a692235ddd29ce5a0241f5e2ee8937e04e16fa0d
Al Wilkinson <alastair.wilkinson@wkconsulting.com.au>
2024-10-07 18:06:01 +1000

    Fix indentation

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-return f"<h1>Hello world, Flask on Azure Web App!</h1><hr/>Current clock time is: {datetime.datetime.utcnow()}"
+    return f"<h1>Hello world, Flask on Azure Web App!</h1><hr/>Current clock time is: {datetime.datetime.utcnow()}"
 
 
 
